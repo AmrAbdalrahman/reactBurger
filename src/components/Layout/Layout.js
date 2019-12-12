292049e243c1e676574c5cb02c594609b76a52ed
AmrAhmed94 <amr.ahmed@fekretyonline.com>
2019-12-12 16:55:55 +0200

    init sidebar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, {Component} from "react";
 
 import Aux from "../../hoc/Auxiliary";
 import classes from './Layout.module.css';
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = (props) => (
-    <Aux>
-        <div>Too;bal, seideDrawer,Back Drop</div>
+class Layout extends Component {
 
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+    side
 
-export default layout;
+    render() {
+        return (
+            <Aux>
+                <Toolbar/>
+                <SideDrawer/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+
+    }
+
+}
+
+export default Layout;
