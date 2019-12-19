020ddc5d7981fe2e3718a0ae3eb094a57594eda8
AmrAhmed94 <amr.ahmed@fekretyonline.com>
2019-12-19 12:37:36 +0200

    works with routing

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -12,8 +12,7 @@ class Checkout extends Component {
     };
 
 
-    componentDidMount() {
-
+    componentWillMount() {
         const query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
         let price = 0;
@@ -44,7 +43,7 @@ class Checkout extends Component {
                 <CheckoutSummary ingredients={this.state.ingredients}
                                  checkoutCancelled={this.checkoutCancelledHandler}
                                  checkoutContinued={this.checkoutContinuedHandler}/>
-                <Route path={this.props.match.path + '/contact-data'} render={() => (<ContactData ingredients={this.state.ingredients}/>) }/>
+                <Route path={this.props.match.path + '/contact-data'} render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props}/>) }/>
             </div>
         )
     }
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,7 +1,9 @@
 import React, {Component} from "react";
 
 import Button from "../../../components/UI/Button/Button";
+import Spinner from "../../../components/UI/Spinner/Spinner";
 import classes from './Contact.module.css';
+import axios from '../../../axios-orders';
 
 class ContactData extends Component {
 
@@ -11,26 +13,57 @@ class ContactData extends Component {
         address: {
             street: '',
             postalCode: ''
-        }
+        },
+        loading: false
     };
 
     orderHandler = (event) => {
         event.preventDefault();
         console.log(this.props.ingredients);
+        this.setState({loading: true});
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: 'Amr Ahmed',
+                address: {
+                    street: 'officer blocks',
+                    zipcode: '2234',
+                    country: 'Egypt'
+                },
+                email: 'amr@amr.com'
+            },
+            deliveryMethod: 'fastest'
+        };
+        axios.post('/orders.json', order)
+            .then(response => {
+                console.log(response);
+                this.setState({loading: false});
+                this.props.history.push('/');
+            }).catch(error => {
+            console.log(error);
+            this.setState({loading: false});
+        });
 
     };
 
     render() {
+        let form = (
+            <form action="">
+                <input className={classes.Input} type="text" name="name" placeholder="Your Name"/>
+                <input className={classes.Input} type="email" name="email" placeholder="Your Mail"/>
+                <input className={classes.Input} type="text" name="street" placeholder="Street"/>
+                <input className={classes.Input} type="text" name="postal" placeholder="Postal Code"/>
+                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            </form>
+        );
+        if (this.state.loading) {
+            form = <Spinner/>;
+        }
         return (
             <div className={classes.ContactData}>
                 <h4>Enter your Contact Data</h4>
-                <form action="">
-                    <input className={classes.Input} type="text" name="name" placeholder="Your Name"/>
-                    <input className={classes.Input} type="email" name="email" placeholder="Your Mail"/>
-                    <input className={classes.Input} type="text" name="street" placeholder="Street"/>
-                    <input className={classes.Input} type="text" name="postal" placeholder="Postal Code"/>
-                    <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-                </form>
+                {form}
             </div>
         );
     }
