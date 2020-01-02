b5e620de5a3d2272098062a48ea1eac3cf46b514
AmrAhmed94 <amr.ahmed@fekretyonline.com>
2020-01-02 13:07:52 +0200

    working with redux saga

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,5 +1,4 @@
 import * as actionTypes from './actionTypes';
-import axios from '../../axios-orders';
 
 export const purchaseBurgerSuccess = (id, orderData) => {
     return {
@@ -24,14 +23,10 @@ export const purchaseBurgerStart = () => {
 };
 
 export const purchaseBurger = (orderData, token) => {
-    return dispatch => {
-        dispatch(purchaseBurgerStart());
-        axios.post('/orders.json?auth=' + token, orderData)
-            .then(response => {
-                dispatch(purchaseBurgerSuccess(response.data.name, orderData))
-            }).catch(error => {
-            dispatch(purchaseBurgerFail(error));
-        });
+    return {
+        type: actionTypes.PURCHASE_BURGER,
+        orderData: orderData,
+        token: token
     }
 };
 
@@ -59,28 +54,16 @@ export const fetchOrdersFail = (error) => {
 
 export const fetchOrdersStart = () => {
     return {
-      type: actionTypes.FETCH_ORDERS_START
+        type: actionTypes.FETCH_ORDERS_START
     };
 };
 
-//run async code
-export const fetchOrders= (token, userId) => {
-    return dispatch => {
-        dispatch(fetchOrdersStart());
-        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
-        axios.get( '/orders.json' + queryParams)
-            .then((res => {
-                const fetchedOrders = [];
-                for (let key in res.data) {
-                    fetchedOrders.push({
-                        ...res.data[key],
-                        id: key
-                    });
-                }
-                dispatch(fetchOrdersSuccess(fetchedOrders));
-            })).catch(err => {
-            dispatch(fetchOrdersFail(err));
-        });
-    }
+//run async code by saga
+export const fetchOrders = (token, userId) => {
+    return {
+        type: actionTypes.FETCH_ORDERS,
+        token: token,
+        userId: userId
+    };
 };
 
